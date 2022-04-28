96e2be357804c7e4b08ab96f6c35da819d9d539c
Rafael Santana <jrafaeldesantana@gmail.com>
2022-04-27 21:08:56 -0300

    Correção questao 3

--- Questao3.asm
+++ Questao3.asm
@@ -4,9 +4,14 @@ lw x11, v2
 addi x18, x10, 0
 
 srli x18, x18, 8
+andi x18, x18, 0xff
 slli x18, x18, 24
+
+slli x11, x11, 8
+srli x11, x11, 8
 
 or x11, x11, x18
 
-v1: .word 0x1234
-v2: .word 0xffff
+v1: .word 0x12345678
+v2: .word 0xfffffff
+
