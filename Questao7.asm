65e99913cc798f55a4c1f966ed1ad6d0fac37f44
Rafael Santana <jrafaeldesantana@gmail.com>
2022-04-29 00:14:12 -0300

    Questao 7

--- Questao7.asm
+++ Questao7.asm
@@ -1,14 +1,49 @@
-add x18, x0, x0						#Resultado
-addi x19, x0, 2						#Numero
-addi x11, x0, 
+add x18, x0, x0						#Resultado Final
+add x10, x0, x0						#Numero Atual
 
-Loop:
-	lb x10, 1025(x0)
+Read:
+	lb x29, 1025(x0)
+	beq x29, x0, End
+	addi x10, x29, -48
+	jal x1, Fatorial
+	add x18, x18, x10
+
+	lb x29, 1025(x0)
+	beq x29, x0, End
+	addi x10, x29, -48
+	jal x1, Fatorial
+	add x18, x18, x10
+
+	lb x29, 1025(x0)
+	beq x29, x0, End
+	addi x10, x29, -48
+	jal x1, Fatorial
+	add x18, x18, x10
+
+	lb x29, 1025(x0)
+	beq x29, x0, End
+	addi x10, x29, -48
+	jal x1, Fatorial
+	add x18, x18, x10
+
+	lb x29, 1025(x0)
+	beq x29, x0, End
+	addi x10, x29, -48
+	jal x1, Fatorial
+	add x18, x18, x10
+
+	lb x29, 1025(x0)
+	beq x29, x0, End
+	addi x10, x29, -48
+	jal x1, Fatorial
+	add x18, x18, x10
+		
+End: halt	
 	
 Fatorial:
 	addi sp, sp, -16
-	sb x1, 8(sp)
-	sb x10, 0(sp)
+	sw x1, 8(sp)
+	sw x10, 0(sp)
 	addi x5, x10, -1
 	bge x5, x0, L1
 	addi x10, x0, 1
@@ -18,9 +53,22 @@ L1:
 	addi x10, x10, -1
 	jal x1, Fatorial
 	addi x6, x10, 0
-	lb x10, 0(sp)
-	lb x1, 8(sp)
+	lw x10, 0(sp)
+	lw x1, 8(sp)
 	addi sp, sp, 16
-	muldiv x10, x10, 
-	
+    jal x5, Multiplicar
+    jalr x0, 0(x1)
 
+Multiplicar:
+	add x25, x0, x10
+	add x26, x0, x0
+
+	MultLoop:
+		beq x25, x0, MultEnd
+		add x26, x26, x6
+		addi x25, x25, -1
+		beq x0, x0, MultLoop
+
+	MultEnd:
+		addi x10, x26, 0 
+		jalr x0, 0(x5)
